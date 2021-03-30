12394c2d10a950d09f89c284b47c6ff649f80af5
nuochong <nuo.chong@163.com>
2021-03-30 11:11:13 +0800

    fix: lang icon

--- src/components/SelectLang/index.jsx
+++ src/components/SelectLang/index.jsx
@@ -1,9 +1,10 @@
 import './index.less'
 
-import { Icon, Menu, Dropdown } from 'ant-design-vue'
+// import { Icon, Menu, Dropdown } from 'ant-design-vue'
+import { Menu, Dropdown } from 'ant-design-vue'
 import { i18nRender } from '@/locales'
 import i18nMixin from '@/store/i18n-mixin'
-// import iconLang from './lang.svg'
+import IconLang from './lang-svg'
 
 // const locales = ['zh-CN', 'zh-TW', 'en-US', 'pt-BR']
 const locales = ['zh-CN', 'en-US']
@@ -33,7 +34,7 @@ const SelectLang = {
     }
   },
   components: {
-    // iconLang
+    IconLang
   },
   name: 'SelectLang',
   mixins: [i18nMixin],
@@ -57,11 +58,8 @@ const SelectLang = {
     return (
       <Dropdown overlay={langMenu} placement="bottomRight" overlayStyle={{ width: this.isMobile ? '100%' : 'auto' }}>
         <span class={prefixCls}>
-        {/* <iconLang></iconLang> */}
-        {/* <svg viewBox="0 0 24 24" focusable="false" width="1em" height="1em" fill="currentColor" aria-hidden="true">
-          <path d="M0 0h24v24H0z" fill="none"></path><path d="M12.87 15.07l-2.54-2.51.03-.03c1.74-1.94 2.98-4.17 3.71-6.53H17V4h-7V2H8v2H1v1.99h11.17C11.5 7.92 10.44 9.75 9 11.35 8.07 10.32 7.3 9.19 6.69 8h-2c.73 1.63 1.73 3.17 2.98 4.56l-5.09 5.02L4 19l5-5 3.11 3.11.76-2.04zM18.5 10h-2L12 22h2l1.12-3h4.75L21 22h2l-4.5-12zm-2.62 7l1.62-4.33L19.12 17h-3.24z " class="css-c4d79v"></path>
-        </svg> */}
-          <Icon type="global" title={i18nRender('navBar.lang')} />
+          <IconLang title={i18nRender('navBar.lang')}></IconLang>
+          {/* <Icon type="global" title={i18nRender('navBar.lang')} /> */}
         </span>
       </Dropdown>
     )
